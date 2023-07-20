17b2d2bec10c8ef93a292a9a93006a9ddbf4b432
Diego <reisdiego144@gmail.com>
2023-07-20 15:20:17 -0300

    alinhando o texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <section>
-          <header className="w-full flex flex-col items-center  h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+          <header className="w-full flex flex-col items-center justify-center h-[36rem] bg-gradient-to-r from-blue-500 to-purple-600">
             <div className="flex flex-col align-center justify-center items-center p-16">
               <h1 className=" text-center text-5xl text-white font-bold drop-shadow-lg">Bem-vind@ ao 
                 <span className="text-cyan-900"> DACOMP</span>
