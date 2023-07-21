8fffa012979dd31efad16e3511094f46e30bce5c
Diego <diegovarella42@byronsolutions.com>
2023-07-21 10:52:01 -0300

    adicionando responsivo e imagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Writer  from '../app/Writer'
+import Image from 'next/image'
 import ButtonDown  from '../app/ButtonDown'
 import Curses from '../app/Curses'
 
@@ -6,15 +7,30 @@ export default function Home() {
   return (
     <main>
       <section>
-          <header className="w-full flex flex-col items-center justify-center h-[36rem] bg-gradient-to-r from-dark-blue to-light-blue ">
-            <div className="flex flex-col align-center justify-center items-center p-16">
-              <h1 className=" text-center text-5xl text-white font-bold drop-shadow-lg">Bem-vind@ ao 
-                <span className="text-cyan-900"> DACOMP</span>
+          <header className="w-full flex flex-col justify-evenly h-[35rem]  bg-gradient-to-r from-dark-blue to-light-blue ">
+            <div className='flex md:justify-evenly  justify-center'>
+            <div className="flex flex-col justify-center items-center p-16">
+              <h1 className=" text-center text-5xl text-cyan-700 font-bold drop-shadow-lg">Bem-vind@ ao 
+                <span className="text-white"> DACOMP</span>
               </h1>
               <Writer/>
             </div>
-          <ButtonDown/>  
-          </header>
+
+            <Image className=' hidden md:block'
+              src={'/logo.png'}
+              alt='Logo da Dacomp'
+              width={500}
+              height={600}
+              priority
+            />
+
+          </div>
+        
+        <div className='flex justify-center items-center'>
+          <ButtonDown/>
+        </div>
+
+        </header>
       </section>
 
       <Curses/>
