fb37c3e1f3245edf7757dc5914c691971b94bb2a
Diego <diegovarella42@byronsolutions.com>
2023-07-23 21:31:08 -0300

    componentizando toda a seçao de cursos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Writer  from '../components/Writer'
 import Image from 'next/image'
 import ButtonDown  from '../components/ButtonDown'
-import Curses from '../components/Curses'
+import Courses from '../components/Courses'
 
 export default function Home() {
   return (
     <main>
       <section>
-          <header className="w-full flex flex-col justify-evenly h-[35rem]  bg-gradient-to-r from-dark-blue to-light-blue ">
+          <header className="w-full flex flex-col justify-evenly h-[35rem]  bg-gradient-to-r from-black from-10% to-light-blue ">
             <div className='flex md:justify-evenly  justify-center'>
             <div className="flex flex-col justify-center items-center p-16">
               <h1 className="text-center text-4xl text-cyan-700 font-bold drop-shadow-lg">Bem-vind@ ao 
@@ -33,13 +33,7 @@ export default function Home() {
         </header>
       </section>
 
-      <div className='flex-col'>
-        <div className='flex justify-center items-center'>
-          <h3 className='text-gray-600 shadow text-4xl'>Cursos</h3>
-        </div>
-
-        <Curses/>
-      </div>
+      <Courses/>
     </main>
   )
 }
